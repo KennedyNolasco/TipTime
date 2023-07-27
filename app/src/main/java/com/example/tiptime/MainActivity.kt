658503d6336ae3a103567900d15d388ddc2c987f
Kennedy Nolasco <blazefenrir@gmail.com>
2023-07-27 17:18:41 -0400

    Added icons and improved visual clarity and acessibility.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import java.util.Locale
@@ -15,31 +19,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         mostrarGorjeta(0.0)
-        binding.calculateButton.setOnClickListener{
+        binding.calculateButton.setOnClickListener {
             calcularGorjeta()
         }
-
+        binding.costOfServiceTextedit.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
-    private fun mostrarGorjeta(gorjeta: Double){
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    private fun mostrarGorjeta(gorjeta: Double) {
         val ptBr = Locale("pt", "BR");
         val gorjetaFormatada = NumberFormat.getCurrencyInstance(ptBr).format(gorjeta)
-        binding.tipResult.text = getString(R.string.valor_da_gorjeta,gorjetaFormatada)
+        binding.tipResult.text = getString(R.string.valor_da_gorjeta, gorjetaFormatada)
     }
-    private fun calcularGorjeta(){
-        val GorjetaString = binding.costOfService.text
+
+    private fun calcularGorjeta() {
+        val GorjetaString = binding.costOfServiceTextedit.text
         val custo = GorjetaString.toString().toDoubleOrNull()
-        if(custo==null){
+        if (custo == null) {
             mostrarGorjeta(0.0)
-           return
+            return
         }
-        val porcentagem = when(binding.tipOptions.checkedRadioButtonId){
+        val porcentagem = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.twenty_percent -> 0.20
             R.id.eighteen_percent -> 0.18
-            else ->0.15
+            else -> 0.15
         }
-        var gorjeta = custo*porcentagem
+        var gorjeta = custo * porcentagem
 
-        if( binding.roundUpSwitch.isChecked) {
+        if (binding.roundUpSwitch.isChecked) {
             gorjeta = kotlin.math.ceil(gorjeta)
         }
         mostrarGorjeta(gorjeta)
